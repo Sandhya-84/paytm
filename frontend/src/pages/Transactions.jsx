ee1cab48f48bd67aeaba3be9375a6de8be8cdb25
sandhya kuppili <sandhyakuppili.84@gmail.com>
2026-06-19 21:16:31 +0530

    improve transaction UI

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
-
+import { jwtDecode } from "jwt-decode";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const Transactions = () => {
     const [transactions, setTransactions] = useState([]);
     const navigate=useNavigate();
+     const token = localStorage.getItem("token");
+ const currentUserId = token ? String(jwtDecode(token).id) : null;
     useEffect(() => {
         axios.get(
             `${BACKEND_URL}/api/v1/account/transactions`,
@@ -45,26 +47,47 @@ export const Transactions = () => {
             </div>
 
 
+{transactions.map(txn => {
+  const isSent = String(txn.sender?._id) === currentUserId;
+const isReceived = String(txn.receiver?._id) === currentUserId;
 
-            {transactions.map(txn => (
-                <div
-                    key={txn._id}
-                    className="border p-4 rounded mb-3 shadow-sm"
-                >
-                  <div className="font-semibold">
-                        {txn.sender?.firstName} {txn.sender?.lastName}
-                        {" → "}
-                        {txn.receiver?.firstName} {txn.receiver?.lastName}
-                    </div>
-                    <div className="mt-1">
-                        ₹{txn.amount}
-                    </div>
-
-                    <div className="text-sm text-gray-500 mt-1">
-                        {new Date(txn.createdAt).toLocaleString()}
-                    </div>
+    return (
+        <div
+            key={txn._id}
+            className="border p-4 rounded mb-3 shadow-sm flex justify-between items-center"
+        >
+            {/* LEFT SIDE */}
+            <div>
+                <div className="font-semibold">
+                    {txn.sender?.firstName} {txn.sender?.lastName}
+                    {" → "}
+                    {txn.receiver?.firstName} {txn.receiver?.lastName}
                 </div>
-            ))}
+
+                <div className="text-sm text-gray-500 mt-1">
+                    {new Date(txn.createdAt).toLocaleString()}
+                </div>
+            </div>
+
+            {/* RIGHT SIDE */}
+           <div className="text-right">
+    <div className="font-bold">₹{txn.amount}</div>
+
+    {isSent && (
+        <span className="inline-block mt-1 px-2 py-1 text-xs rounded-full bg-red-100 text-red-600">
+            Sent
+        </span>
+    )}
+
+    {isReceived && (
+        <span className="inline-block mt-1 px-2 py-1 text-xs rounded-full bg-green-100 text-green-700">
+            Received
+        </span>
+    )}
+</div>
+        </div>
+    );
+})}
         </div>
     );
 };
