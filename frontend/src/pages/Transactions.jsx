f71c133188f06717a9338b3de93ff6fe3549559e
sandhya kuppili <sandhyakuppili.84@gmail.com>
2026-06-19 20:58:19 +0530

    add dashboard route after transaction history

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const Transactions = () => {
     const [transactions, setTransactions] = useState([]);
-
+    const navigate=useNavigate();
     useEffect(() => {
         axios.get(
             `${BACKEND_URL}/api/v1/account/transactions`,
@@ -23,10 +25,26 @@ export const Transactions = () => {
     }, []);
 
     return (
+
         <div className="m-8">
+            <div className="justify-between flex item-center">
             <h1 className="text-2xl font-bold mb-4">
                 Transaction History
             </h1>
+
+           <button
+    onClick={() => navigate("/dashboard")}
+    className="flex items-center gap-2 bg-gradient-to-r from-blue-500 to-blue-600 
+               text-white px-4 py-2 rounded-lg shadow-md 
+               hover:from-blue-600 hover:to-blue-700 
+               transition-all duration-200 active:scale-95"
+>
+    <FiArrowLeft className="text-lg" />
+    Dashboard
+</button>
+            </div>
+
+
 
             {transactions.map(txn => (
                 <div
